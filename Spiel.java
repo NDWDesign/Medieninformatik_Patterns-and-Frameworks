554cbd9d8d8f1a101c7e8ecdf6104fe2ba7cf065
Nils Daniel Wittwer <nils.wittwer@ndwdesign.net>
2016-10-23 11:14:58 +0200

    Vorlage für Aufgabe 1

---
+++ Spiel.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Spiel {
+    public static void main(String[] args) {
+        while (true) {
+            // Zahl einlesen
+            int eingabe = new Scanner(System.in).nextInt();
+
+            System.out.println("Zahl eingegeben: "+eingabe);
+        }
+    }
+}
